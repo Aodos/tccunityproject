3ca0d14f59cad26f0dd4eb1ead9e18d798822627
Aodos <arthur.tng.marques@gmail.com>
2019-06-17 22:31:41 -0300

    Projeto Unity TCC

---
+++ Assets/Orbita.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbita : MonoBehaviour
+{
+    GameObject sol;
+    public Transform center;
+    public Vector3 axis = Vector3.up;
+    public Vector3 desiredPosition;
+    public float radius;
+    public float radiusSpeed = 0.5f;
+    public float rotationSpeed = 80.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sol = GameObject.FindWithTag("sol");
+        center = sol.transform;
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+        // radius = 5.0f;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
+        desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+    }
+}
